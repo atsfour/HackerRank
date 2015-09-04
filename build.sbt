a1bb645161cfda4ded8d021c44120aa241c3c0b9
atsfour <atsfour@users.noreply.github.com>
2015-09-04 13:59:22 +0900

    first commit

---
+++ build.sbt
@@ -0,0 +1,6 @@
+name := "hackerRank"
+
+version := "1.0"
+
+scalaVersion := "2.11.7"
+    
---
+++ src/main/scala/StringCompression.scala
@@ -0,0 +1,5 @@
+object StringCompression extends App{
+  val sc = new java.util.Scanner(System.in)
+  val word = sc.next
+
+}
